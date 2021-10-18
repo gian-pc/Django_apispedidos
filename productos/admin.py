1c0792eaf9a65991c1ac0c9526f46d8078a14210
Gian Carlos <gpaucarcortez@gmail.com>
2021-10-18 16:51:50 -0500

    Models Productos

--- productos/admin.py
+++ productos/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import Categoria
+from .models import Categoria, Productos
 
-admin.site.register(Categoria)
-
+admin.site.register([Categoria, Productos])
--- productos/models.py
+++ productos/models.py
@@ -9,3 +9,22 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.nombre
+
+
+class Productos(models.Model):
+    codigo = models.CharField(max_length=10, primary_key=True)
+    producto = models.CharField(max_length=140)
+    descripcion = models.TextField(blank=True, null=True)
+    caracteristicas = models.CharField(max_length=140,blank=True, null=True)
+    estado = models.BooleanField(default=True)
+    fecha_creacion = models.DateTimeField(blank=True, null=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE,related_name='categorias')
+
+    def __str__(self):
+        return self.producto
+
+    class Meta:
+        verbose_name_plural='productos'
+
+
+
