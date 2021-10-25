ede41d0693d7fe136f8933867ea99e8ec994cb82
Gian Carlos <gpaucarcortez@gmail.com>
2021-10-24 23:09:45 -0500

    Try Catch

--- productos/urls.py
+++ productos/urls.py
@@ -1,9 +1,8 @@
-
-
 from django.urls import path
-from .views import index
+from .views import index, detalles_productos
 
 urlpatterns = [
 
-    path('', index)
+    path('', index, name='index'),
+    path('detalles/<codigo>', detalles_productos, name='detalles')
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Productos
+from django.http import Http404
 
 
 def index(request):
@@ -7,3 +8,12 @@ def index(request):
     productos = Productos.objects.all()
     context = {'prod': productos}
     return render(request, "index.html", context)
+
+
+def detalles_productos(request, codigo):
+    try:
+        productos_id = Productos.objects.get(codigo=codigo)
+        context = {'prod': productos_id}
+    except Productos.DoesNotExist:
+        raise Http404("Producto No Existe. Lo sentimos")
+    return render(request, "productodetalle.html", context)
