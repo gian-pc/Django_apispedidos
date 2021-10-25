b7eb13a3f79baedfad2a0fcb482986cb81974c10
Gian Carlos <gpaucarcortez@gmail.com>
2021-10-25 15:23:23 -0500

    finished

--- productos/urls.py
+++ productos/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import index, detalles_productos,filtrar_productos
+from .views import index, detalles_productos, filtrar_productos, productos_fotos, list_all_platos, list_all_platos_procedure,find_plato
 
 urlpatterns = [
 
     path('', index, name='index'),
     path('detalles/<codigo>', detalles_productos, name='detalles'),
-    path('filtrar/<nombre>', filtrar_productos, name='filtrar')
+    path('filtrar/<nombre>', filtrar_productos, name='filtrar'),
+    path('productosfotos', productos_fotos, name='productosfotos'),
+    path('platosql/', list_all_platos, name='platosql'),
+    path('platosproc/', list_all_platos_procedure, name='platosproc'),
+    path('findplato/<cod>', find_plato, name='findplato')
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .models import Productos
+from .models import Productos, Productos_Fotos
 from django.http import Http404
+from django.db import connection
 
 
 def index(request):
@@ -23,3 +24,44 @@ def filtrar_productos(request, nombre):
     busqueda = Productos.objects.filter(nombre__contains=nombre)
     context = {'prod': busqueda}
     return render(request, "productos.html", context)
+
+
+def productos_fotos(request):
+    platos = Productos_Fotos.objects.all()
+    context = {'prod': platos}
+    return render(request, "productosfotos.html", context)
+
+
+# MYSQL DIRECTAMENTE
+def list_all_platos(request):
+    with connection.cursor() as cursor:
+        cursor.execute("select * from productos_productos")
+        rowdata = cursor.fetchall()
+        result = []
+        for item in rowdata:
+            result.append(list(item))
+        context = {'prod': result}
+    return render(request, 'productosql.html', context)
+
+
+# EJECUTANDO EL PROCEDIMIENTO
+def list_all_platos_procedure(request):
+    with connection.cursor() as cursor:
+        cursor.callproc("usp_productos")
+        rowdata = cursor.fetchall()
+        result = []
+        for item in rowdata:
+            result.append(list(item))
+        context = {'prod': result}
+    return render(request, 'productosqlproc.html', context)
+
+
+def find_plato(request, cod):
+    with connection.cursor() as cursor:
+        cursor.callproc("usp_productos_fotos", [cod])
+        rowdata = cursor.fetchall()
+        result = []
+        for item in rowdata:
+            result.append(list(item))
+        context = {'prod': result}
+    return render(request, 'productosqlproc.html', context)
